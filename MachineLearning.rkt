8e568c30c46c9e81f7c9d9f9f79baaa1053b5d93
guptariti <55262402+guptariti@users.noreply.github.com>
2020-01-01 12:22:31 -0500

    Update MachineLearning.rkt

--- MachineLearning.rkt
+++ MachineLearning.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname MachineLearning) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; A Pixel is a [0-255]
 ; A NatNum between 0 and 255 representing a shade of grey.
 (define PIXEL-WHITE 0)
